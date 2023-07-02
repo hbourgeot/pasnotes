76f3dc137bac30941f0cdc50f201c9d96e3c7508
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-07-01 23:57:36 -0400

    añadida funcionalidad a tabla de docentes

--- src/routes/docentes/(private)/+page.server.ts
+++ src/routes/docentes/(private)/+page.server.ts
@@ -1,5 +1,7 @@
 import type { PageServerLoad } from "./$types";
 
-export const load = (async ({ locals: { docente } }) => {
-  return { docente };
+export const load = (async ({ locals: {client, docente } }) => {
+  const { ok, status, data } = await client.GET(`/api/docente/${docente.cedula}`);
+  if (!ok) return { docente: null, materias: [] }
+  return { docente: data.docente, materias: data.materias };
 }) satisfies PageServerLoad;
--- src/routes/docentes/(private)/+page.svelte
+++ src/routes/docentes/(private)/+page.svelte
@@ -2,6 +2,7 @@
   import { Table, tableMapperValues } from "@skeletonlabs/skeleton";
   import type { TableSource } from "@skeletonlabs/skeleton";
   import type {PageData} from "./$types"
+  import type { Materia } from "../../../app";
 
   export let data: PageData;
   
@@ -11,34 +12,25 @@
     let table = {
         cantidad_estudiantes: 24
     }
-    const sourceData = [
-    { materia: "Introducción a la administración", estudiantes: table.cantidad_estudiantes},
-  ];
+    const sourceData = data.materias.map((materia: Materia) => ({codigo: materia.id, nombre: materia.nombre, estudiantes: table.cantidad_estudiantes}));
 
   const tableSimple: TableSource = {
-    // A list of heading labels.
-    head: ["Materia", "Estudiantes asignados"],
-    // The data visibly shown in your table body UI.
-    body: tableMapperValues(sourceData, ["materia", "estudiantes"]),
+    head: ["Código", "Nombre", "Estudiantes asignados"],
+    body: tableMapperValues(sourceData, ["codigo", "nombre", "estudiantes"]),
   };
+
+  const handleSelection = (e: CustomEvent) => {
+    console.log(e.detail);
+  }
 
 </script>
 
 <main class='w-full h-screen'>
-    <section class='flex flex-col gap-12'>
+    <section class='flex flex-col gap-5'>
         <h2 class='text-3xl text-left pl-6 pt-12 lg:pt-16 lg:pl-48'>Bienvenid@, {nombre_profesor}!</h2>
-        <div class='flex container h-auto rounded-md'>
-             <section class='w-full mb-8'>
-                <h3 class='text-2xl pl-4 py-4'>Acciones</h3>
-              <ul class='flex justify-around items-center w-full'>
-                <li>Materias</li>
-                <li>Estudiantes</li>
-                <li><a href="docentes/informacion">Informacion</a></li>
-              </ul>
-             </section>
-        </div>
+        <h2 class="text-xl text-left pl-10 mt-10">Materias asignadas: </h2>
 
-        <Table source={tableSimple} class='mx-auto w-[98%] text-center lg:w-[80%]'> </Table>
+        <Table interactive={true} on:selected={handleSelection} source={tableSimple} class='mx-auto w-[98%] text-center lg:w-[80%]'> </Table>
 
     </section>
 </main>
