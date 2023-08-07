c342a2dc772fb175ba7af6e9c421c875df7a9a67
Samuel <samuel.suarez@wimbu.co>
2023-08-06 23:14:39 -0400

    Add icon

--- src/routes/(sistemas)/docentes/(private)/+layout.svelte
+++ src/routes/(sistemas)/docentes/(private)/+layout.svelte
@@ -4,7 +4,7 @@
   import {type ModalComponent, Toast, Modal, AppBar, type ModalSettings, modalStore } from "@skeletonlabs/skeleton";
   import ModalForm from "$lib/components/ModalFormNotas.svelte";
   import type { LayoutData } from "./$types";
-  import { ChevronRight } from "@steeze-ui/material-design-icons";
+  import { ChevronRight, Logout } from "@steeze-ui/material-design-icons";
   import { Icon } from "@steeze-ui/svelte-icon";
 
   export let data: LayoutData;
@@ -94,8 +94,10 @@
     <form method="post" action="{`/docentes/logout?nombre=${data.nombre}`}">
       <button
         type="submit"
-        class="bg-pink-600 text-gray-200 px-3 py-1 rounded-3xl ml-4 h-[50px] text-light-50"
-        >Cerrar sesión</button
+        class="bg-pink-600 text-gray-200 px-3 py-1 rounded-2xl ml-4 h-[50px] text-light-50"
+        > 
+        <Icon class='w-8 h-8' src={Logout}/>
+        </button
       >
     </form>
   </svelte:fragment>
