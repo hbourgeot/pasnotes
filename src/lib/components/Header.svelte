afed70b7756d6f68798e01058a662c9df5db6338
xRayderx <josemarti1896@gmail.com>
2023-09-17 14:51:25 -0400

    changes in landing page

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,13 @@
 <script>
   import Logo from "$lib/images/logo.jpg";
   import { Icon } from "@steeze-ui/svelte-icon";
-  import { Menu } from "@steeze-ui/material-design-icons";
+  import { Menu, ArrowDropDown } from "@steeze-ui/material-design-icons";
+
+  import { AppBar } from '@skeletonlabs/skeleton';
+  import { TreeView, TreeViewItem } from "@skeletonlabs/skeleton";
 </script>
 
-<nav
+<!--<nav
   class="w-full h-[80px] bg-white relative cursor-default flex justify-center items-center"
   style="border-bottom: 1px solid rgb(229 231 235);
 "
@@ -35,7 +38,114 @@
       </ul>
     </div>
   </details>
-</nav>
+</nav>-->
+
+<!--<div class="bg-white w-full h-[80px] relative flex justify-between items-center px-4 md:px-0" style="border-bottom: 1px solid rgb(229 231 235);">
+  <div class="md:hidden w-full text-center">
+    <img src={Logo} class="inline-block w-12 h-12 md:w-15 md:h-15" alt="logo" />
+  </div>
+  <div class="hidden md:block">
+    <img src={Logo} class="w-12 h-12 md:w-15 md:h-15" alt="logo" />
+  </div>
+  
+  <details class="md:hidden absolute w-full z-10">
+    <summary>
+      <Icon src={Menu} class="ml-4 w-10 h-10 ease" />
+    </summary>
+    <div style="width: 100%" class="w-full mt-4 h-auto nav bg-gray-900 text-white">
+      <ul class="w-full h-full">
+        <li>
+          <a href="/estudiantes/login">Estudiantes</a>
+        </li>
+        <li>
+          <a href="/docentes/login">Docentes</a>
+        </li>
+        <li>
+          <a href="/control_estudio/login">Control de Estudio</a>
+        </li>
+        <li>
+          <a href="/coordinadores/login">Coordinacion</a>
+        </li>
+      </ul>
+    </div>
+  </details>
+
+  <nav class="hidden md:flex space-x-4">
+    <a href="/estudiantes/login">Estudiantes</a>
+    <a href="/docentes/login">Docentes</a>
+    <a href="/control_estudio/login">Control de Estudio</a>
+    <a href="/coordinadores/login">Coordinacion</a>
+  </nav>
+</div>-->
+
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end" background="bg-white" padding="py-4" style="border-bottom: 1px solid rgb(229 231 235);">
+	<svelte:fragment slot="lead">
+    <a href="/" class="hidden xl:block">
+      <img src={Logo} class="ml-4 w-12 h-12 md:w-15 md:h-15" alt="logo" />
+    </a>
+
+    <details class="xl:hidden absolute w-full z-10">
+      <summary>
+        <Icon src={Menu} class="ml-4 w-10 h-10 ease" />
+      </summary>
+      <div style="width: 100%" class="w-full mt-5 h-auto nav bg-gray-900 text-white">
+        <ul class="w-full h-full">
+          <li>
+            <a href="/estudiantes/login">Estudiantes</a>
+          </li>
+          <li>
+            <a href="/docentes/login">Docentes</a>
+          </li>
+          <li>
+            <a href="/control_estudio/login">Control de Estudio</a>
+          </li>
+          <li>
+            <a href="/coordinadores/login">Coordinacion</a>
+          </li>
+        </ul>
+      </div>
+    </details>
+  </svelte:fragment>
+	
+  <a href="/" class="xl:hidden w-full text-center">
+    <img src={Logo} class="inline-block w-12 h-12 md:w-15 md:h-15" alt="logo" />
+  </a>
+
+	<svelte:fragment slot="trail">
+    <div class="xl:block hidden relative mr-4">
+      <TreeView>
+        <TreeViewItem>
+          Iniciar Sesión
+  
+          <svelte:fragment slot="children">
+            <div class="absolute bg-white z-10 mt-4 w-full rounded-sm p-2">
+              <TreeViewItem regionSymbol="hidden">
+                <span class="w-full text-center">
+                  <a href="/estudiantes/login">Estudiantes</a>
+                </span>
+              </TreeViewItem>
+              <TreeViewItem regionSymbol="hidden">
+                <span class="w-full text-center">
+                  <a href="/docentes/login">Docentes</a>
+                </span>  
+              </TreeViewItem>
+              <TreeViewItem regionSymbol="hidden">
+                <span class="w-full text-center">
+                  <a href="/control_estudio/login">Control de Estudio</a>
+                </span>
+              </TreeViewItem>
+              <TreeViewItem regionSymbol="hidden">
+                <span class="w-full text-center">
+                  <a href="/coordinadores/login">Coordinacion</a>
+                </span>
+              </TreeViewItem>
+            </div>
+          </svelte:fragment>
+        </TreeViewItem>
+      </TreeView>
+    </div> 
+  </svelte:fragment>
+</AppBar>
 
 <style>
   summary::marker {
